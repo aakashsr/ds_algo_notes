c9cc38fcca00a795073100a2c6d136d1437144a4
aakashsr <aakashsri.dev@gmail.com>
2020-06-04 09:36:46 +0530

    updated O(1)

--- Big O/example1.js
+++ Big O/example1.js
@@ -1,3 +1,5 @@
+// -----------------  O(N)  -------------------- //
+
 const nemo = ["nemo"];
 
 const allElements = new Array(10).fill("nemo");
@@ -10,7 +12,8 @@ function findNemo1(array) {
   }
 }
 
-// So , the big of the for loops is O(1) becaus if we'll have 10 elements , we have to loop through all the elements to find the required element.
+// So , the bigO of the for loops is O(n) because if we'll have 10 elements , we have to loop through all the elements to find the required element.
+// bigO of while loop is also O(n)
 
 // ForEach also has linear time i,e O(n) because inside it , it's implementing the same logic i,e for loop
 
@@ -27,3 +30,28 @@ function findNemo2(array) {
 }
 
 findNemo1(allElements);
+
+// -----------------  O(1)  -------------------- //
+
+const big = new Array(1000).fill("name");
+
+function findNemo3(array) {
+  console.log(array[0]); // O(1)
+}
+
+function findNemo4(array) {
+  console.log(array[0]); // O(1)
+  console.log(array[1]); // O(1)
+}
+
+function findNemo5(array) {
+  console.log(array[0]); // O(1)
+  console.log(array[1]); // O(1)
+  console.log(array[2]); // O(1)
+}
+
+findNemo3(big); // bigO = O(1)
+findNemo4(big); // bigO  = O(1)
+findNemo5(big); // bigO  = O(1)
+
+// In above cases , doesn't matter what the size of array is , the number of operations are fixed . So , we'll get constant line not linear.
