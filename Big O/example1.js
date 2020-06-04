54abb6673813ef6945296589db5fb0236dd79183
aakashsr <aakashsri.dev@gmail.com>
2020-06-04 09:48:05 +0530

    updated first two  exercies

--- Big O/example1.js
+++ Big O/example1.js
@@ -55,3 +55,19 @@ findNemo4(big); // bigO  = O(1)
 findNemo5(big); // bigO  = O(1)
 
 // In above cases , doesn't matter what the size of array is , the number of operations are fixed . So , we'll get constant line not linear.
+
+// EXERCISE 1 - Calculate bigO :
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < input.length; i++) {
+    // O(n)
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
+  }
+  return a; // O(1)
+}
+
+// bigO of above function => O(n)
