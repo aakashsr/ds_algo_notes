86f81dde62701edd71f5ab5d70791c3b2f622cc6
aakashsr <aakashsri.dev@gmail.com>
2020-06-09 09:57:14 +0530

    added the best and fourth solution to google question

--- How to solve coding problems/example2.js
+++ How to solve coding problems/example2.js
@@ -73,61 +73,59 @@ console.log(commonItem3(arr1, arr2));
 
 
 
-
 /* -------------- GOOGLE INTERVIEW QUESTION ----------------- */
 
 // Q: Finding pair of numbers in an array whose sum is equal to given sum.
 
 let array1 = [1, 2, 3, 9];
-let array2 = [1, 7, 8, 10];
+let array2 = [1,2, 4,4];
 
 /* -------------- METHOD 1 ----------------- */
 
 // Method 1 - Naive Approach
-// Iterating over each number and checking whehter their sum is equal to given sum or not.
+// Iterating over each number and checking whether their sum is equal to given sum or not.
 // Complexity : O(n^2)
 
-// function checkPairSum1(arr, sum) {
-//   for(let i = 0 ; i < arr.length ; i++){
-//     for(let j = 0 ; j < arr.length ; j++){
-//       if(i !== j && arr[i] + arr[j] === sum){
-//         return true;
-//       }
-//     }
-//   }
-//   return false;
-// }
+function checkPairSum1(arr, sum) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j && arr[i] + arr[j] === sum) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
-// console.log(checkPairSum1(array2,8));
+console.log(checkPairSum1(array2,8));
 
-//  Method 2 
+//  METHOD 2  
 // Complexity : O(n)
 
-// function checkPairSum2(arr, sum) {
-//   let counter = arr.length - 1;
-//   for (let i = 0; i < arr.length; i++) {
-//     if ((arr[i] + arr[counter]) === sum) {
-//       console.log(arr[i], arr[counter])
-//       return true;
-//     }
+function checkPairSum2(arr, sum) {
+  let counter = arr.length - 1;
+  for (let i = 0; i < arr.length; i++) {
+    if ((arr[i] + arr[counter]) === sum) {
+      return true;
+    }
+    if (i === counter) {
+      counter--;
+      i = 0;
+    }
+  }
+  return false;
+}
 
-//     if (i === counter) {
-//       counter--;
-//       i = 0;
-//     }
-//   }
-//   return false;
-// }
+console.log(checkPairSum2(array2, 8));
 
-// console.log(checkPairSum2(array2, 8));
-
+// METHOD 3 - Using while loop
+// Complexity : O(n)
 
 function checkPairSum3(arr, sum) {
   let high = arr.length - 1;
   let low = 0;
   while (low < high) {
     if (arr[low] + arr[high] === sum) {
-      console.log(arr[low], arr[high])
       return true;
     } else {
       low++;
@@ -141,3 +139,20 @@ function checkPairSum3(arr, sum) {
 }
 
 console.log(checkPairSum3(array2, 8));
+
+// METHOD 4 - Using Set and Complement of Value 
+// Complexity : Constant Time 
+
+function checkPairSum4(arr, sum) {
+  const mySet = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (mySet.has(arr[i])) {
+      return true;
+    } else {
+      mySet.add(sum - arr[i]);
+    }
+  }
+  return false;
+}
+
+console.log(checkPairSum4(array2, 8));
