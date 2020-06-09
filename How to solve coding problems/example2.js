7d7bc36f4fade26d1a533971903c1cadf90bf6ba
aakashsr <aakashsri.dev@gmail.com>
2020-06-09 09:27:48 +0530

    added 3 solutions to google interview question

--- How to solve coding problems/example2.js
+++ How to solve coding problems/example2.js
@@ -27,19 +27,19 @@ function commonItem2(arr1, arr2) {
   // 1. Converted array into object
   let newObject = convertArrayIntoObject(arr1);
   // 2. Iterate throguh second array and check if any items exist as property on first or not.
-  matchItem(arr2,newObject);
+  matchItem(arr2, newObject);
   return false;
 }
 
 function convertArrayIntoObject(arr) {
-  let newObject = {};                   // As JS is a lecical scoped language , so place of function definition decide the scope not place of function execution
-  arr.forEach((cur) => {                // that's why we need to return "newObject" from "convertArrayIntoObject" and then pass the same into "matchItem".So that , both have access to same object
+  let newObject = {}; // As JS is a lecical scoped language , so place of function definition decide the scope not place of function execution
+  arr.forEach((cur) => { // that's why we need to return "newObject" from "convertArrayIntoObject" and then pass the same into "matchItem".So that , both have access to same object
     newObject[cur] = true;
   })
   return newObject;
 }
 
-function matchItem(arr,newObject) {
+function matchItem(arr, newObject) {
   arr.forEach((cur) => {
     if (newObject[cur]) {
       console.log('matched2');
@@ -58,11 +58,86 @@ commonItem2(arr1, arr2);
 
 // Using JS Utilities ( Array.some method )
 
-function commonItem3(arr1,arr2){
+function commonItem3(arr1, arr2) {
   return arr1.some((item) => arr2.includes(item));
 }
 
-console.log(commonItem3(arr1,arr2));
+console.log(commonItem3(arr1, arr2));
 
 
 /* ---------------------------------------- */
+
+
+
+
+
+
+
+
+/* -------------- GOOGLE INTERVIEW QUESTION ----------------- */
+
+// Q: Finding pair of numbers in an array whose sum is equal to given sum.
+
+let array1 = [1, 2, 3, 9];
+let array2 = [1, 7, 8, 10];
+
+/* -------------- METHOD 1 ----------------- */
+
+// Method 1 - Naive Approach
+// Iterating over each number and checking whehter their sum is equal to given sum or not.
+// Complexity : O(n^2)
+
+// function checkPairSum1(arr, sum) {
+//   for(let i = 0 ; i < arr.length ; i++){
+//     for(let j = 0 ; j < arr.length ; j++){
+//       if(i !== j && arr[i] + arr[j] === sum){
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+// console.log(checkPairSum1(array2,8));
+
+//  Method 2 
+// Complexity : O(n)
+
+// function checkPairSum2(arr, sum) {
+//   let counter = arr.length - 1;
+//   for (let i = 0; i < arr.length; i++) {
+//     if ((arr[i] + arr[counter]) === sum) {
+//       console.log(arr[i], arr[counter])
+//       return true;
+//     }
+
+//     if (i === counter) {
+//       counter--;
+//       i = 0;
+//     }
+//   }
+//   return false;
+// }
+
+// console.log(checkPairSum2(array2, 8));
+
+
+function checkPairSum3(arr, sum) {
+  let high = arr.length - 1;
+  let low = 0;
+  while (low < high) {
+    if (arr[low] + arr[high] === sum) {
+      console.log(arr[low], arr[high])
+      return true;
+    } else {
+      low++;
+      if (low === high) {
+        low = 0;
+        high--;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(checkPairSum3(array2, 8));
